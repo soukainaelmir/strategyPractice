1eefa7a53c09d169b4da7f88d767636b95bfd3e2
soukaina <soukaina.elmir.93@gmail.com>
2023-01-02 23:51:37 -0700

    store class created

---
+++ src/Store.java
@@ -0,0 +1,7 @@
+public class Store {
+    public static void main(String[] args) {
+        System.out.println("zara");
+        System.out.println("gucci");
+        System.out.println("LV");
+    }
+}
